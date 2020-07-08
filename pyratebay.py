d8b7e6d111262cb29a20ad9e8c3dca20b8149857
jedi2610 <krithickumarub@protonmail.com>
2020-07-08 20:36:53 +0530

    Refactored code and fixed bugs

--- pyratebay.py
+++ pyratebay.py
@@ -9,13 +9,14 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from prettytable import *
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import InvalidArgumentException
+from selenium.common.exceptions import InvalidArgumentException, WebDriverException
 from selenium.webdriver.chrome.options import Options
 
 os.environ['WDM_LOG_LEVEL'] = '20'
 
+
 def print_banner():
-    return ("""
+    print("""
                                                                                                          __                       _______                      
                                                                                                         /  |                     /       \                     
                                                                  ______   __    __   ______   ______   _$$ |_     ______         $$$$$$$  |  ______   __    __ 
@@ -30,41 +31,26 @@ def print_banner():
                                                                 $$/        $$$$$$/                                                                    $$$$$$/  
     """)
 
-def findPirateSite():
-    """
-    Finds a functional piratebay site
 
-    @return: returns the URL of the functional piratebay site
-    """
-    try:
-        siteList = requests.get("https://piratebay-proxylist.net")
-        siteList.raise_for_status()
-    except Exception as e:
-        print(e ,)
-        exit(0)
-
-    # Obtain the URL of the working piratebay site
-    soup = BeautifulSoup(siteList.text, features="lxml")
-    countries = soup.select(".country")
-    domains = soup.select(".domain")
-    for country in countries:
-        if country.getText().strip() == "IN":
-            index = countries.index(country)
-            domain = domains[index].getText()
-            break
-
-    return domain
-
-
-def main(args):
+def initialize_driver():
 
     os.environ['WDM_LOG_LEVEL'] = '0'
     chromeOptions = Options()
     chromeOptions.add_argument('--headless')
     chromeOptions.add_argument('--disable-extensions')
     chromeOptions.add_argument('--disable-gpu')
-    chromeOptions.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chromeOptions)
+    chromeOptions.add_experimental_option(
+        'excludeSwitches', ['enable-logging'])
+    try:
+        driver = webdriver.Chrome(
+            ChromeDriverManager().install(), options=chromeOptions)
+    except WebDriverException:
+        print("Chrome app is unresponsive.")
+        exit(0)
+    return driver
+
+
+def resize_screen():
 
     pl = sys.platform
     if pl == 'win32':
@@ -73,24 +59,46 @@ def main(args):
     elif pl == 'linux' or pl == 'linux2' or pl == 'darwin':
         os.system("clear")
         os.system("resize -s 300 300")
-    print(print_banner())
+    print_banner()
 
+
+def findPirateSite(driver, isDebug):
+    """
+    Finds a functional piratebay site
+
+    @return: returns the URL of the functional piratebay site
+    """
     # Finds a functional piratebay site
-    domain = findPirateSite()
+    if isDebug:
+        print('Inside findPirateSite() function')
+    try:
+        driver.get("https://piratebay-proxylist.net")
+        if isDebug:
+            print('Scraping data of all working websites for your country')
+        pageData = driver.page_source
+    except Exception as e:
+        print(e)
+        driver.quit()
+        exit(0)
 
-    # 
-    mediaName = ' '.join(args.query)
-    category = args.category
+    # Obtain the URL of the working piratebay site
+    soup = BeautifulSoup(pageData, features="lxml")
+    countries = soup.select(".country")
+    domains = soup.select(".url")
+    index = indianDomains = list()
+    for country in countries:
+        if country.getText().strip() == "IN":
+            index = countries.index(country)
+            countries[index] = ""
+            indianDomains.append(domains[index].getText().strip())
 
-    types = ["all", "audio", "video", "apps", "games", "other"]
-    parameters = {}
+    return indianDomains
 
-    # URL encoding and finding the URL of the final page
-    for i in types:
-        if i in category:
-            parameters.update({i: "on"})
 
-    
+def processFinalPage(driver, domain, mediaName, parameters, isDebug):
+
+    if isDebug:
+        print('Inside processFinalPage() function')
     query = urllib.parse.quote_plus(mediaName)
     finalUrl = "https://{0}/search.php?q={1}".format(domain, query)
     for key, value in parameters.items():
@@ -122,19 +130,33 @@ def main(args):
     table.hrules = ALL
     table.padding_width = 4
 
-    for i in range(0, totEntries+1):
-        table.add_row([i, typeOfMedia[i].getText(), nameOfMedia[i].getText(), uploadDate[i].getText(),
-                    sizeOfMedia[i].getText(), uploadedBy[i].getText(), seeders[i].getText(), leechers[i].getText()])
+    try:
+        for i in range(0, totEntries + 1):
+            table.add_row([i, typeOfMedia[i].getText(), nameOfMedia[i].getText(), uploadDate[i].getText(
+            ), sizeOfMedia[i].getText(), uploadedBy[i].getText(), seeders[i].getText(), leechers[i].getText()])
+    except IndexError as e:
+        if isDebug:
+            print(f'Error while scraping data from {domain}')
+        return None
 
     table.del_row(0)
     print("\n\n")
     print(table)
 
-    userInput = int(input("\nWhich torrent do you wanna download? (0 to exit) > "))
+    try:
+        userInput = int(
+            input("\nWhich torrent do you wanna download? (0 to exit) > "))
+    except ValueError:
+        print('Input type should be integer')
+        driver.quit()
+        exit(0)
+
     if 0 < userInput <= 100:
         path = nameOfMedia[userInput].a.get('href')
         finalUrl = "https://" + domain + path
     else:
+        print('Not a valid option')
+        driver.quit()
         exit(0)
 
     driver.get(finalUrl)
@@ -145,7 +167,8 @@ def main(args):
 
     try:
         print('\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
-        print("\n", "Description of the torrent: \n\n", descriptionText[0].getText())
+        print("\n", "Description of the torrent: \n\n",
+              descriptionText[0].getText())
     except:
         pass
 
@@ -154,22 +177,58 @@ def main(args):
         print("\nList of files included in the torrent: \n")
         maxLen = max([len(fileName.getText()) for fileName in fileNames])
         for i in range(0, len(fileNames)):
-            print(fileNames[i].getText().ljust(maxLen, " "), "\t", fileSizes[i].getText())
+            print(fileNames[i].getText().ljust(
+                maxLen, " "), "\t", fileSizes[i].getText())
     except:
         pass
-    
+
+    return magnetLinks[userInput-1].get('href')
+
+
+def main(args):
+
+    isDebug = args.debug
+    driver = initialize_driver()
+    resize_screen()
+    domains = findPirateSite(driver, isDebug)
+    if isDebug:
+        print('List of domains:', ', '.join(domains))
+        print('Total working domains:', len(domains))
+
+    mediaName = ' '.join(args.query)
+    category = args.category
+    types = ["all", "audio", "video", "apps", "games", "other"]
+    parameters = {}
+    for i in types:
+        if i in category:
+            parameters.update({i: "on"})
+
+    for domain in domains:
+        if isDebug:
+            print("Currently checking domain: ", domain)
+        magnetLink = processFinalPage(
+            driver, domain, mediaName, parameters, isDebug)
+        if magnetLink is not None:
+            break
+
+        if domain == domains[-1]:
+            print("Oops!, couldn't find any working sites for your country.")
+
+    # webbrowser.open(magnetLink)
+    if isDebug:
+        print(magnetLink)
     driver.quit()
-    
-    webbrowser.open(magnetLinks[userInput-1].get('href'))
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('query', nargs='+', type=str, metavar='query',
-                        help="Name of the media to download")
+    parser.add_argument('query', nargs='+', type=str,
+                        metavar='query', help="Name of the media to download")
     parser.add_argument('-c', '--category', default="all", choices=[
                         "all", "audio", "video", "apps", "games", "other"], nargs='*', dest='category', type=str, help="Searches for the given 'name' in the specified category (default = all)")
+    parser.add_argument('-d', '--debug', dest='debug',
+                        action='store_true', help="Prints debug messages")
     args = parser.parse_args()
 
     main(args)
